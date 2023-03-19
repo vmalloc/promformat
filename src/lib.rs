ffd3b4c238727ca735489ebdd890e926a530c3eb
Rotem Yaari <vmalloc@gmail.com>
2023-03-19 22:54:28 +0200

    Initial version

---
+++ src/lib.rs
@@ -0,0 +1,108 @@
+use std::fmt::Write;
+
+pub struct Metrics {
+    buffer: String,
+}
+
+impl Metrics {
+    pub fn new() -> Self {
+        Self {
+            buffer: Default::default(),
+        }
+    }
+
+    pub fn gauge<'a>(&'a mut self, name: &str, help: &str) -> MetricGroup<'a> {
+        self.metric_group("gauge", name, help)
+    }
+
+    pub fn counter<'a>(&'a mut self, name: &str, help: &str) -> MetricGroup<'a> {
+        self.metric_group("counter", name, help)
+    }
+
+    fn metric_group<'a>(
+        &'a mut self,
+        type_: &'static str,
+        name: &str,
+        help: &str,
+    ) -> MetricGroup<'a> {
+        MetricGroup::new(self, type_, name, help)
+    }
+
+    pub fn render(&self) -> &str {
+        &self.buffer
+    }
+}
+
+pub struct MetricGroup<'a> {
+    metrics: &'a mut Metrics,
+    name: String,
+}
+
+impl<'a> MetricGroup<'a> {
+    fn new(metrics: &'a mut Metrics, metric_type: &str, name: &str, help: &str) -> Self {
+        writeln!(&mut metrics.buffer, "# HELP {name} {help}").unwrap();
+        writeln!(&mut metrics.buffer, "# TYPE {name} {metric_type}").unwrap();
+        Self {
+            name: name.to_owned(),
+            metrics,
+        }
+    }
+
+    pub fn label(&mut self, label: &str, value: &str) -> SingleMetric {
+        SingleMetric {
+            name: &self.name,
+            metrics: self.metrics,
+            labels: String::default(),
+        }
+        .label(label, value)
+    }
+}
+
+pub struct SingleMetric<'a, 'b> {
+    labels: String,
+    name: &'a String,
+    metrics: &'b mut Metrics,
+}
+
+impl<'a, 'b> SingleMetric<'a, 'b> {
+    pub fn label(mut self, label: &str, value: &str) -> Self {
+        if self.labels.is_empty() {
+            self.labels.push_str("{")
+        } else {
+            self.labels.push_str(",")
+        }
+
+        // TODO: escaping
+        write!(&mut self.labels, "{label}=\"{value}\"").unwrap();
+
+        self
+    }
+
+    pub fn set(mut self, value: impl std::fmt::Display) {
+        self.labels.push_str("}");
+        writeln!(self.metrics.buffer, "{}{} {value}", self.name, self.labels).unwrap();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_gauge() {
+        let mut metrics = Metrics::new();
+
+        let mut gauge = metrics.gauge("testme", "help here");
+        gauge.label("x", "y").set(2);
+        gauge.label("a", "b").label("c", "d").set(20);
+
+        assert_eq!(
+            metrics.render(),
+            r#"# HELP testme help here
+# TYPE testme gauge
+testme{x="y"} 2
+testme{a="b",c="d"} 20
+"#
+        );
+    }
+}
